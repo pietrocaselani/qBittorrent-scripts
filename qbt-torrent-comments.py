b0a64239a8711e9db3549a588c62a269d3c6e664
Pietro Caselani <pc1992@gmail.com>
2025-05-25 07:15:51 -0300

    Add script to display comments from torrents

---
+++ qbt-torrent-comments.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3 -u
+
+import sys
+from qbt_utils import *
+from datetime import datetime
+
+try:
+    fix_encoding()
+    client = get_client()
+
+    for torrent in client.torrents.info():
+        if torrent.comment and torrent.comment.strip():
+            print(f"Torrent '{torrent.name}' has a comment: {torrent.comment}")
+except Exception as e:
+    print(f"An error occurred: {e}")
